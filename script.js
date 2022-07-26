c88865a28ab3d27b9e1db0347843b40ac7e1318e
Tiago Carmo <87225951+ReduxFlakes@users.noreply.github.com>
2022-07-26 11:46:08 +0100

    Fluent Blur Deisign Update

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    const toggleButton = document.getElementsByClassName('navbar-button')[0];
-    const navbarLinks = document.getElementsByClassName('navbar-links');
-    toggleButton.addEventListener('click', function() {
-        for (var i = 0; i < navbarLinks.length; i++)
-            navbarLinks[i].classList.toggle('active');
-    });
-});
